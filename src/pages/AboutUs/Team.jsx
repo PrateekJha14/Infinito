f3f27d90685a3b3553cd2a39b20d59f57d4881d9
Rudra <rudralm10@gmail.com>
2024-09-05 21:17:48 +0530

    Team update

--- src/pages/AboutUs/Team.jsx
+++ src/pages/AboutUs/Team.jsx
@@ -1,66 +1,66 @@
-import styles from "./Team.module.css";
+import styles from './Team.module.css';
 // import image from "../assets/langchain_logo.jpeg";
-import pawan from "../assets/pawan.png";
-import ritik from "../assets/ritik sharma.png";
-import aryan from "../assets/aryan.png";
-import himanshu from "../assets/himanshu.png";
-import mayank from "../assets/mayank.png";
-import harsh from "../assets/harsh.png";
-import rudra from "../assets/rudra.png";
-import aaditya from "../assets/aaditya.png";
-import akhilesh from "../assets/akhilesh.png";
-import gaurav from "../assets/gaurav verma.png";
-import manish from "../assets/manish.png";
-import saumya from "../assets/saumya.png";
-import tanishq from "../assets/tanishq.png";
-import jagriti from "../assets/jagriti.png";
-import phutela from "../assets/phutela.png";
-import sakshi from "../assets/sakshi.png";
-import abhinav from "../assets/abhinav.png";
-import arti from "../assets/arti.png";
-import ansh from "../assets/ansh.png";
-import dolly from "../assets/dolly.png";
-import pullah from "../assets/pulah.png";
-import anirudh from "../assets/anirudh.png";
-import bhavik from "../assets/bhavik.png";
-import rishi from "../assets/rishi.jpg";
-import shani from "../assets/shani.jpg";
-import aman from "../assets/aman.png";
-import arun from "../assets/arun.jpg";
-import sanchay from "../assets/sanchay.jpg";
-import mojammi from "../assets/mojammi.jpg";
-import sumit from "../assets/sumit.png";
-import mohit from "../assets/mohit.jpg";
-import Footer from "../../components/Footer";
-import Navbar from "../../components/Navbar";
-import shyam from "../assets/shyam.jpg";
-import raunak from "../assets/raunak.jpg";
-import balwant from "../assets/balwant.jpg";
+import pawan from '../assets/pawan.png';
+import ritik from '../assets/ritik sharma.png';
+import aryan from '../assets/aryan.jpg';
+import himanshu from '../assets/himanshu.png';
+import mayank from '../assets/mayank.png';
+import harsh from '../assets/harsh.png';
+import rudra from '../assets/rudra.png';
+import aaditya from '../assets/aaditya.png';
+import akhilesh from '../assets/akhilesh.png';
+import gaurav from '../assets/gaurav verma.png';
+import manish from '../assets/manish.png';
+// import saumya from '../assets/saumya.png';
+import tanishq from '../assets/tanishq.png';
+import jagriti from '../assets/jagriti.png';
+import phutela from '../assets/phutela.png';
+import sakshi from '../assets/sakshi.png';
+import abhinav from '../assets/abhinav.png';
+import arti from '../assets/arti.png';
+import ansh from '../assets/ansh.png';
+import dolly from '../assets/dolly.png';
+import pullah from '../assets/pulah.png';
+import anirudh from '../assets/anirudh.png';
+import bhavik from '../assets/bhavik.png';
+import rishi from '../assets/rishi.jpg';
+import shani from '../assets/shani.jpg';
+import aman from '../assets/aman.png';
+import arun from '../assets/arun.jpg';
+import sanchay from '../assets/sanchay.jpg';
+import mojammi from '../assets/mojammi.jpg';
+import sumit from '../assets/sumit.png';
+import mohit from '../assets/mohit.jpg';
+import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
+import shyam from '../assets/shyam.jpg';
+import raunak from '../assets/raunak.jpg';
+import balwant from '../assets/balwant.jpg';
 
 const teamMembers = [
   {
-    role: "Overall Head Coordinator",
-    name: "Balwant Meena",
+    role: 'Overall Head Coordinator',
+    name: 'Balwant Meena',
 
-    position: "Fest Coordinator",
+    position: 'Fest Coordinator',
     image: balwant,
   },
   {
-    role: "Managing Lead Coordinator",
-    name: "Anirudh Singh",
-    position: "Managing Lead",
+    role: 'Managing Lead Coordinator',
+    name: 'Anirudh Singh',
+    position: 'Managing Lead',
     image: anirudh,
   },
   {
-    role: "Events Coordinators",
-    name: "Pulah Panara",
-    position: "Events Lead",
+    role: 'Events Coordinators',
+    name: 'Pulah Panara',
+    position: 'Events Lead',
     image: pullah,
   },
   {
-    role: "Events Coordinators",
-    name: "Shyam Sundar",
-    position: "Events Lead",
+    role: 'Events Coordinators',
+    name: 'Shyam Sundar',
+    position: 'Events Lead',
     image: shyam,
   },
   // Media & Public Relations Coordinators
@@ -71,46 +71,46 @@ const teamMembers = [
   //   image: image,
   // },
   {
-    role: "Media & Public Relations Coordinators",
-    name: "Arun Mishra",
-    position: "Media & Public Relations",
+    role: 'Media & Public Relations Coordinators',
+    name: 'Arun Mishra',
+    position: 'Media & Public Relations',
     image: arun,
   },
   {
-    role: "Media & Public Relations Coordinators",
-    name: "Raunak Bansod",
-    position: "Media & Public Relations",
+    role: 'Media & Public Relations Coordinators',
+    name: 'Raunak Bansod',
+    position: 'Media & Public Relations',
     image: raunak,
   },
   {
-    role: "Media & Public Relations Coordinators",
-    name: "Mohit Saini",
-    position: "Media & Public Relations",
+    role: 'Media & Public Relations Coordinators',
+    name: 'Mohit Saini',
+    position: 'Media & Public Relations',
     image: mohit,
   },
   {
-    role: "Media & Public Relations Coordinators",
-    name: "Shani Kasaudhan",
-    position: "Media & Public Relations",
+    role: 'Media & Public Relations Coordinators',
+    name: 'Shani Kasaudhan',
+    position: 'Media & Public Relations',
     image: shani,
   },
   {
-    role: "Media & Public Relations Coordinators",
-    name: "Dolly Singh",
-    position: "Media & Public Relations",
+    role: 'Media & Public Relations Coordinators',
+    name: 'Dolly Singh',
+    position: 'Media & Public Relations',
     image: dolly,
   },
   // Creatives & Design Coordinators
   {
-    role: "Creatives & Design Coordinators",
-    name: "Ansh Singh",
-    position: "Creatives & Design",
+    role: 'Creatives & Design Coordinators',
+    name: 'Ansh Singh',
+    position: 'Creatives & Design',
     image: ansh,
   },
   {
-    role: "Creatives & Design Coordinators",
-    name: "Arti Dewangan",
-    position: "Creatives & Design",
+    role: 'Creatives & Design Coordinators',
+    name: 'Arti Dewangan',
+    position: 'Creatives & Design',
     image: arti,
   },
   // TV Team Coordinators
@@ -121,151 +121,151 @@ const teamMembers = [
   //   image: image,
   // },
   {
-    role: "TV Team Coordinators",
-    name: "Abhinav Vanamala",
-    position: "TV Team",
+    role: 'TV Team Coordinators',
+    name: 'Abhinav Vanamala',
+    position: 'TV Team',
     image: abhinav,
   },
   // Sponsorship Coordinators
   {
-    role: "Sponsorship Coordinators",
-    name: "Sakshi Sinha",
-    position: "Sponsorship",
+    role: 'Sponsorship Coordinators',
+    name: 'Sakshi Sinha',
+    position: 'Sponsorship',
     image: sakshi,
   },
   {
-    role: "Sponsorship Coordinators",
-    name: "Rishi Panwar",
-    position: "Sponsorship",
+    role: 'Sponsorship Coordinators',
+    name: 'Rishi Panwar',
+    position: 'Sponsorship',
     image: rishi,
   },
   {
-    role: "Sponsorship Coordinators",
-    name: "Ansh Phutela",
-    position: "Sponsorship",
+    role: 'Sponsorship Coordinators',
+    name: 'Ansh Phutela',
+    position: 'Sponsorship',
     image: phutela,
   },
   {
-    role: "Sponsorship Coordinators",
-    name: "Jagriti Kanwar",
-    position: "Sponsorship",
+    role: 'Sponsorship Coordinators',
+    name: 'Jagriti Kanwar',
+    position: 'Sponsorship',
     image: jagriti,
   },
 
   {
-    role: "Sponsorship Coordinators",
-    name: "Tanishq Sharma",
-    position: "Sponsorship",
+    role: 'Sponsorship Coordinators',
+    name: 'Tanishq Sharma',
+    position: 'Sponsorship',
     image: tanishq,
   },
   {
-    role: "Sponsorship Coordinators",
-    name: "Mojammi Warsi",
-    position: "Sponsorship",
+    role: 'Sponsorship Coordinators',
+    name: 'Mojammi Warsi',
+    position: 'Sponsorship',
     image: mojammi,
   },
 
-  {
-    role: "Sponsorship Coordinators",
-    name: "Saumya Pratap Singh",
-    position: "Sponsorship",
-    image: saumya,
-  },
+  // {
+  //   role: "Sponsorship Coordinators",
+  //   name: "Saumya Pratap Singh",
+  //   position: "Sponsorship",
+  //   image: saumya,
+  // },
   // Events & Management Coordinators
   {
-    role: "Events & Management Coordinators",
-    name: "Aman Kumar",
-    position: "Events & Management",
+    role: 'Events & Management Coordinators',
+    name: 'Aman Kumar',
+    position: 'Events & Management',
     image: aman,
   },
 
   {
-    role: "Events & Management Coordinators",
-    name: "Manish Kumar",
-    position: "Events & Management",
+    role: 'Events & Management Coordinators',
+    name: 'Manish Kumar',
+    position: 'Events & Management',
     image: manish,
   },
   {
-    role: "Events & Management Coordinators",
-    name: "Gaurav Verma",
-    position: "Events & Management",
+    role: 'Events & Management Coordinators',
+    name: 'Gaurav Verma',
+    position: 'Events & Management',
     image: gaurav,
   },
   {
-    role: "Events & Management Coordinators",
-    name: "Akhilesh Ingole",
-    position: "Events & Management",
+    role: 'Events & Management Coordinators',
+    name: 'Akhilesh Ingole',
+    position: 'Events & Management',
     image: akhilesh,
   },
   {
-    role: "Events & Management Coordinators",
-    name: "Aaditya Kaswan",
-    position: "Events & Management",
+    role: 'Events & Management Coordinators',
+    name: 'Aaditya Kaswan',
+    position: 'Events & Management',
     image: aaditya,
   },
   // Web Development Coordinators
   {
-    role: "Web Development Coordinators",
-    name: "Rudra Goyal",
-    position: "Web Development",
+    role: 'Web Development Coordinators',
+    name: 'Rudra Goyal',
+    position: 'Web Development',
     image: rudra,
   },
 
   {
-    role: "Web Development Coordinators",
-    name: "Pawan Singh",
-    position: "Web Development",
+    role: 'Web Development Coordinators',
+    name: 'Pawan Singh',
+    position: 'Web Development',
     image: pawan,
   },
   // Registration Coordinators
   {
-    role: "Registration Coordinators",
-    name: "Harsh Mittal",
-    position: "Registration",
+    role: 'Registration Coordinators',
+    name: 'Harsh Mittal',
+    position: 'Registration',
     image: harsh,
   },
   {
-    role: "Registration Coordinators",
-    name: "Mayank Sen",
-    position: "Registration",
+    role: 'Registration Coordinators',
+    name: 'Mayank Sen',
+    position: 'Registration',
     image: mayank,
   },
   {
-    role: "Registration Coordinators",
-    name: "Himanshu Chakravarty",
-    position: "Registration",
+    role: 'Registration Coordinators',
+    name: 'Himanshu Chakravarty',
+    position: 'Registration',
     image: himanshu,
   },
   // E-sports Coordinators
   {
-    role: "E-sports Coordinators",
-    name: "Aryan",
-    position: "E-sports",
+    role: 'E-sports Coordinators',
+    name: 'Aryan',
+    position: 'E-sports',
     image: aryan,
   },
   {
-    role: "E-sports Coordinators",
-    name: "Sumit Sumhan",
-    position: "E-sports",
+    role: 'E-sports Coordinators',
+    name: 'Sumit Sumhan',
+    position: 'E-sports',
     image: sumit,
   },
   {
-    role: "E-sports Coordinators",
-    name: "Ritik Sharma",
-    position: "E-sports",
+    role: 'E-sports Coordinators',
+    name: 'Ritik Sharma',
+    position: 'E-sports',
     image: ritik,
   },
   // Hospitality Coordinators
   {
-    role: "Hospitality Coordinators",
-    name: "Bhavik Netam",
-    position: "Hospitality",
+    role: 'Hospitality Coordinators',
+    name: 'Bhavik Netam',
+    position: 'Hospitality',
     image: bhavik,
   },
   {
-    role: "Hospitality Coordinators",
-    name: "Sanchay Rathore",
-    position: "Hospitality",
+    role: 'Hospitality Coordinators',
+    name: 'Sanchay Rathore',
+    position: 'Hospitality',
     image: sanchay,
   },
 
